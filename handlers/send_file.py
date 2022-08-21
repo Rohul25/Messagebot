5116194a2a55899e85dadf867792be6cab8c8334
Mr.Rohul <92625688+Rohul25@users.noreply.github.com>
2022-08-21 16:40:05 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -8,7 +8,7 @@ from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
 
 
-async def reply_forward(message: Message, file_id: int):
+async def reply_forward(message: Message, file_id: int, protect_content=True):
     try:
         await message.reply_text(
             f"**Here is Sharable Link of this file:**\n"
@@ -17,23 +17,23 @@ async def reply_forward(message: Message, file_id: int):
             disable_web_page_preview=True, quote=True)
     except FloodWait as e:
         await asyncio.sleep(e.value)
-        await reply_forward(message, file_id)
+        await reply_forward(message, file_id, protect_content=True)
 
 
-async def media_forward(bot: Client, user_id: int, file_id: int):
+async def media_forward(bot: Client, user_id: int, file_id: int, protect_content=True):
     try:
         if Config.FORWARD_AS_COPY is True:
             return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                          message_id=file_id)
+                                          message_id=file_id, protect_content=True)
         elif Config.FORWARD_AS_COPY is False:
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                              message_ids=file_id)
+                                              message_ids=file_id, protect_content=True)
     except FloodWait as e:
         await asyncio.sleep(e.value)
-        return media_forward(bot, user_id, file_id)
+        return media_forward(bot, user_id, file_id, protect_content=True)
 
 
-async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
+async def send_media_and_reply(bot: Client, user_id: int, file_id: int, protect_content=True):
     sent_message = await media_forward(bot, user_id, file_id)
     #await reply_forward(message=sent_message, file_id=file_id)
-    await asyncio.sleep(3)
+    await asyncio.sleep(4)
